b429cbf8560958a12f070633eabe0384cddccee6
flattWhite <onlyforsocial01@gmail.com>
2019-09-30 22:44:54 -0400

    Added @qualifier custom annotation and properties file

--- core/src/main/java/com/sen/config/GameConfig.java
+++ core/src/main/java/com/sen/config/GameConfig.java
@@ -1,22 +1,32 @@
 package com.sen.config;
 
+import com.sen.GuessCount;
+import com.sen.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // == fields ==
-    private int maxNumber = 50;
-    private int guessCount = 10;
+    @Value("${game.maxNumber:20}")  // 20 is default value if properties file cannot be found
+    private int maxNumber;
+
+    @Value("${game.guessCount:5}")
+    private int guessCount;
 
     // == bean methods ==
     @Bean
+    @MaxNumber
     public int maxNumber(){
         return maxNumber;
     }
 
     @Bean
+    @GuessCount
     public int guessCount(){
         return guessCount;
     }
